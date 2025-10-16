d598e7432633284793ee2b901ee74cfcb9d7a1fe
PraptiH <praptiantara1892@gmail.com>
2025-10-16 20:58:02 +0600

    Save book information in the database

--- index.js
+++ index.js
@@ -26,6 +26,22 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+
+    //books API
+    const booksCollection = client.db('virtualBookshelf').collection('Books')
+
+    app.get('/books',async(req,res)=>{
+        const result = await booksCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.post('/books',async(req,res)=>{
+        const newBook = req.body
+        console.log(newBook)
+        const result = await booksCollection.insertOne(newBook)
+        res.send(result)
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
